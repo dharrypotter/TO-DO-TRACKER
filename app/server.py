44a3dd9cee0ab96a533d3cb8983596260f0e2d64
Dhara Awasthi <60693050+dharrypotter@users.noreply.github.com>
2026-04-30 22:06:35 -0700

    Change default port to 8000 and update debug flag

--- app/server.py
+++ app/server.py
@@ -615,5 +615,7 @@ def dashboard():
 ensure_db()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=os.environ.get('FLASK_DEBUG') == '1')
+    port = int(os.environ.get('PORT', 8000))
+    debug = os.environ.get('FLASK_DEBUG', '0') == '1'
+
+    app.run(host='0.0.0.0', port=port, debug=debug)
